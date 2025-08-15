7b398996b530fd1860bba4c13cc77632b1b891d4
sneha <snehaattk@gmail.com>
2025-08-15 14:49:25 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a=float(input("enter first number"))
+b=float(input("enter second number"))
+op=input("enter operation(+,-,*,/):")
+if op == '+':print(a+b)
+elif op =='-':print(a-b)
+elif op =='*':print(a*b)
+elif op =='/':print(a/b)
+else:
+    print("invalid operator")
+
